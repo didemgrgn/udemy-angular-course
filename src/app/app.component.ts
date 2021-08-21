c22ec6330e3abe24cef377f7344aba174b5b068c
didemgrgn <didemgirgin95@gmail.com>
2021-08-21 20:08:41 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import {NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'udemy-angular-course';
+  constructor(
+    private toastr: ToastrService,
+    private spinner:NgxSpinnerService //spinner intance ı oluşturuldu
+    ) {}
+
+  ngOnInit() {
+     this.showSuccess();
+     this.showSpinner();
+  }
+
+  showSuccess() {
+    this.toastr.success('Udemy Angular 9 projemize hoşgelniz...!', 'Giriş Başarılı!'); //toastr gelir
+  }
+
+  showSpinner(){
+    this.spinner.show(); //Servise girerse bu çalışır.
+
+    setTimeout(()=>{
+      this.spinner.hide(); //Servisten cevap dönene kadar kapama gerçekleşmez.
+    },5000)/**5 saniye sonra kapatıcak */
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; //CUSTOM_ELEMENTS_SCHEMA: Haberdar olmadığı şeyleri çalıştırmak için yazıldı
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr'; //toastr import edildi
+import { NgxSpinnerModule } from "ngx-spinner"; //Spinner import edildi
+import { IgxButtonModule } from 'igniteui-angular';//ignite button modülü import edildi.
 
 @NgModule({
   declarations: [
@@ -10,9 +16,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({ //app modelülün altında tüm rootlarda toastr modülünün çalışacağını belirtir
+      timeOut:4000, //4 saniye çalışıcak
+      progressBar:true,
+      easing:"ease-in",
+      closeButton: false,
+      progressAnimation: "decreasing",
+      preventDuplicates: true,
+      positionClass: "toast-bottom-left"
+    }), // ToastrModule added
+    NgxSpinnerModule, //ngx-spinner import edildi
+    IgxButtonModule //ignite button modülü import edildi.
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]//ngx-spinner import edildi
 })
 export class AppModule { }
