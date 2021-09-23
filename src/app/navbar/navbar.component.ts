668a85e3de0572bd19605194591528dba2744fc8
didemgrgn <didemgirgin95@gmail.com>
2021-09-23 22:52:28 +0300

    ngx

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { UserService } from 'src/libs';
+import { Input,EventEmitter } from '@angular/core';
+
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+ 
+  @Input()  lang: any;
+  @Output() langChange: EventEmitter<any> = new EventEmitter();
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  toggleLogin(){
+  this.userService.login(); //logini toggle lamaya yarar.
   }
 
 }
